ebf42ff66fd355d29ba8d3d6947934dffe44af3d
MonseEstebans <Guadalupe.Esteban@fundacion-jala.org>
2023-03-09 18:27:59 -0600

    Class 3: Bird Task

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 class ObstacleManager:
 
@@ -10,16 +12,26 @@ class ObstacleManager:
     def update(self, game_speed, game):
 
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            type = random.randint(0,2)
+            match type:
+                case 0:
+                    self.obstacles.append(Bird(BIRD))
+                case 1:
+                    self.obstacles.append(Cactus(SMALL_CACTUS))
+                case 2:
+                    self.obstacles.append(Cactus(LARGE_CACTUS))
 
         for obstacle in self.obstacles:
             obstacle.update(game_speed, self.obstacles)
 
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(300)
-                game.playing = False
-                break
-
+                game.heart_manager.reduce_heart()
+                if game.heart_manager.heart_count <1:
+                    pygame.time.delay(300)
+                    game.playing = False
+                    break
+                else:
+                    self.obstacles.remove(obstacle)
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -8,6 +8,7 @@ SCREEN_WIDTH = 1100
 FPS = 30
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
+FONT_ARIAL = pygame.font.match_font('arial')
 # Assets Constants
 ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
 
@@ -70,3 +71,6 @@ BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
 HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
 
 DEFAULT_TYPE = "default"
+SHIELD_TYPE = "shield"
+
+LIVES = 4
